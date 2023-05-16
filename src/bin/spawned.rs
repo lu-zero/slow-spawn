c0c681d1c79735397b7393a90f45e03382e5d5cd
Luca Barbato <lu_zero@gentoo.org>
2023-05-16 12:56:38 +0200

    Make the testcase richer

--- src/bin/spawned.rs
+++ src/bin/spawned.rs
@@ -1,5 +1,12 @@
 fn main() {
-    println!("Hello from the spawned!");
+    let t = std::time::SystemTime::now();
+    let s = std::env::args().nth(1).unwrap();
+    let start = std::time::Duration::from_micros(s.parse().unwrap());
+    let now = t.duration_since(std::time::SystemTime::UNIX_EPOCH).unwrap();
+    println!("Hello from the spawned! {:?}", (now - start));
     std::thread::sleep(std::time::Duration::from_millis(500));
-    println!("Hello from the spawned after sleeping!");
+    println!(
+        "Hello from the spawned after sleeping! {:?}",
+        t.elapsed().unwrap()
+    );
 }
--- src/bin/spawner.rs
+++ src/bin/spawner.rs
@@ -1,12 +1,36 @@
 fn main() {
-    println!("Hello before spawning!");
+    let t = std::time::SystemTime::now();
+    let s = format!(
+        "{}",
+        t.duration_since(std::time::SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_micros()
+    );
+    println!("Hello before spawning! (status)");
 
     std::process::Command::new("./target/release/spawned")
-        .arg("4")
+        .arg(s)
+        .status()
+        .unwrap();
+
+    println!("Hello after spawning! {:?}", t.elapsed().unwrap());
+
+    let t = std::time::SystemTime::now();
+    let s = format!(
+        "{}",
+        t.duration_since(std::time::SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_micros()
+    );
+
+    println!("Hello before spawning! (status + explicit inherit)");
+
+    std::process::Command::new("./target/release/spawned")
+        .arg(s)
         .stdout(std::process::Stdio::inherit())
         .stderr(std::process::Stdio::inherit())
         .status()
         .unwrap();
 
-    println!("Hello after spawning!");
+    println!("Hello after spawning! {:?}", t.elapsed().unwrap());
 }
